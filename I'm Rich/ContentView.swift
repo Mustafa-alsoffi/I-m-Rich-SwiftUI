a811a0e8a455d0ef2c3d54f669bb14b556184b6e
MustafaSwift <alsoffimustafa@gmail.com>
2019-12-21 13:50:32 +0800

    Added a red color to the background

--- I'm Rich/ContentView.swift
+++ I'm Rich/ContentView.swift
@@ -10,7 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+           Color(.systemRed)
+        
+                .edgesIgnoringSafeArea(.all)
+           
+            VStack {
+                Text("I'm Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+                
+            }
+            
+
+                    
+            
+        }
     }
 }
 
